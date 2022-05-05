9516a2936f2bd100e1b1081e133d8ae77d582499
MadhavKrishna09 <krishnamadhav923@gmail.com>
2022-05-05 08:47:58 +0530

    lesson-02

--- app/src/main/java/controller/MainActivity.kt
+++ app/src/main/java/controller/MainActivity.kt
@@ -1,12 +1,29 @@
 package controller
 
+import Services.Dtaservices
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.example.coderswag.R
+import kotlinx.android.synthetic.main.activity_main.*
+import model.Category
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : ArrayAdapter<Category>
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        
+        adapter = ArrayAdapter(this,
+        android.R.layout.simple_expandable_list_item_1,
+        Dtaservices.catagories)
+
+        categorylistview.adapter = adapter
     }
+
+
 }
